6451dca1cfcb140359fb642deeb7a4b79892e561
tobeidat <toqaobeidat1@gmail.com>
2024-02-14 00:46:43 +0300

    mobileApp

--- src/mobileApplicationAppium/testCases.java
+++ src/mobileApplicationAppium/testCases.java
@@ -1,11 +1,10 @@
 package mobileApplicationAppium;
 
 import org.openqa.selenium.By;
-//import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import org.testng.Assert;
-//import org.testng.Assert;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
